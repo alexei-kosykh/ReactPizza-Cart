f2763956979dcdd94714afc5b8380b1a8bb22c6b
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-09-28 14:57:38 +0300

    defaultProps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { Categories, PizzaBlock } from "../components";
 
 export const Home = () => {
   const [pizzaItems, setPizzaItems] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/db.json")
-      .then((resp) => resp.json())
-      .then((json) => {
-        setPizzaItems(json.pizzas);
-        console.log(json.pizzas);
-      });
+    axios
+      .get("http://localhost:3000/db.json")
+      .then(({ data }) => setPizzaItems(data.pizzas));
   }, []);
   return (
     <div className="container">
