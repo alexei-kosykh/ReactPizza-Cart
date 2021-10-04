9102374b74e0ef2019dd6b5042c7d15045e4a486
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-10-04 15:14:05 +0300

    Used useDispatch and useSelector in home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
 
 import { Categories, PizzaBlock } from "../components";
+import { setPizzas } from "../redux/actions/pizzas";
 
 export const Home = () => {
-  const [pizzaItems, setPizzaItems] = useState([]);
+  const dispatch = useDispatch();
+  const { items } = useSelector(({ pizzas }) => {
+    return {
+      items: pizzas.items,
+    };
+  });
   useEffect(() => {
     axios
       .get("http://localhost:3000/db.json")
-      .then(({ data }) => setPizzaItems(data.pizzas));
+      .then(({ data }) => dispatch(setPizzas(data.pizzas)));
   }, []);
   return (
     <div className="container">
       <Categories />
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {pizzaItems.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj} />
-        ))}
+        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
