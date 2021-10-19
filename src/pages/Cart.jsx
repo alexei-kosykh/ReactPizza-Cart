895ea7d34f3af4db8dde837db54df2118b1a5005
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-10-19 11:12:54 +0300

    Adaptation scss (768 and 500  500px) screen

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -45,77 +45,75 @@ export const Cart = () => {
 
   return (
     <div>
-      <div className="content">
-        <div className="container container--cart">
-          {totalCount ? (
-            <div className="cart">
-              <div className="cart__top">
-                <h2 className="content__title">
-                  {cartSvg}
-                  Корзина
-                </h2>
-                <div className="cart__clear">
-                  {urnSvg}
-                  <span onClick={onClearAllCart}>Очистить корзину</span>
-                </div>
-              </div>
-              <div className="content__items">
-                {addedPizzas.map((obj) => (
-                  <CartItem
-                    id={obj.id}
-                    key={obj.id}
-                    name={obj.name}
-                    type={obj.type}
-                    size={obj.size}
-                    imageUrl={obj.imageUrl}
-                    totalPrice={items[obj.id].totalPriceByType}
-                    totalCount={items[obj.id].totalCountByType}
-                    onClickRemove={onRemoveItem}
-                    onClickPlus={onPlusItem}
-                    onClickMinus={onMinusItem}
-                  />
-                ))}
-              </div>
-              <div className="cart__bottom">
-                <div className="cart__bottom-details">
-                  <span>
-                    Всего пицц: <b>{totalCount} шт.</b>
-                  </span>
-                  <span>
-                    Сумма заказа: <b>{totalPrice} р.</b>
-                  </span>
-                </div>
-                <div className="cart__bottom-buttons">
-                  <Link
-                    to="/"
-                    className="button button--outline button--add go-back-btn"
-                  >
-                    {arrowLeftSvg}
-                    <span>Вернуться назад</span>
-                  </Link>
-                  <Button onClick={onPayItem} className="pay-btn">
-                    <span>Оплатить сейчас</span>
-                  </Button>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div className="cart cart--empty">
-              <h2>
-                <i>😱</i> Ваша корзина пустая <i>😱</i>
+      <div className="container container--cart">
+        {totalCount ? (
+          <div className="cart">
+            <div className="cart__top">
+              <h2 className="content__title">
+                {cartSvg}
+                Корзина
               </h2>
-              <p>
-                Необходимо добавить хотя бы один товар 🙏
-                <br />
-                Для заказа пиццы, вам нужно перейди на главную страницу.
-              </p>
-              <img src={empryCartImg} alt="Empty cart" />
-              <Link to="/" className="button button--black">
-                <span>Вернуться назад</span>
-              </Link>
+              <div className="cart__clear">
+                {urnSvg}
+                <span onClick={onClearAllCart}>Очистить корзину</span>
+              </div>
             </div>
-          )}
-        </div>
+            <div className="content__items">
+              {addedPizzas.map((obj) => (
+                <CartItem
+                  id={obj.id}
+                  key={obj.id}
+                  name={obj.name}
+                  type={obj.type}
+                  size={obj.size}
+                  imageUrl={obj.imageUrl}
+                  totalPrice={items[obj.id].totalPriceByType}
+                  totalCount={items[obj.id].totalCountByType}
+                  onClickRemove={onRemoveItem}
+                  onClickPlus={onPlusItem}
+                  onClickMinus={onMinusItem}
+                />
+              ))}
+            </div>
+            <div className="cart__bottom">
+              <div className="cart__bottom-details">
+                <span>
+                  Всего пицц: <b>{totalCount} шт.</b>
+                </span>
+                <span>
+                  Сумма заказа: <b>{totalPrice} р.</b>
+                </span>
+              </div>
+              <div className="cart__bottom-buttons">
+                <Link
+                  to="/"
+                  className="button button--outline button--add go-back-btn"
+                >
+                  {arrowLeftSvg}
+                  <span>Вернуться назад</span>
+                </Link>
+                <Button onClick={onPayItem} className="pay-btn">
+                  <span>Оплатить сейчас</span>
+                </Button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="cart cart--empty">
+            <h2>
+              <i>😱</i> Ваша корзина пустая <i>😱</i>
+            </h2>
+            <p>
+              Необходимо добавить хотя бы один товар 🙏
+              <br />
+              Для заказа пиццы, вам нужно перейди на главную страницу.
+            </p>
+            <img src={empryCartImg} alt="Empty cart" />
+            <Link to="/" className="button button--black">
+              <span>Вернуться назад</span>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
